0f0a7189387f3b705603ca9172228ad9e762c31d
Fellipe Adorno <fellipeac2@gmail.com>
2019-08-08 15:48:50 -0300

    teste

--- irsensor.ts
+++ irsensor.ts
@@ -4,7 +4,7 @@ namespace robox {
     // Functions for reading light from the gatorlight in lux or straight adv value
 
     /**
-    * Reads the number
+    * Reads the number 1
     */
     //% weight=30 blockId="robox_irsensor1" block="Get ir-return1 %command"
     export function irsensor1(): number{
@@ -19,7 +19,7 @@ namespace robox {
 	return value
     }
     /**
-    * Reads the number
+    * Reads the number 2
     */
     //% weight=30 blockId="robox_irsensor2" block="Get ir-return2 %command"
     export function irsensor2(): number{
